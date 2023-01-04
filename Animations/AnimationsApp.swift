f23bd41115cbbde0b11567d8f47d9393f18b639d
KiranJungGurung <kirangurung394@gmail.com>
2023-01-04 09:01:39 +0545

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kiran Gurung on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
